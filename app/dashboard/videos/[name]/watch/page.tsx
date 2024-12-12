377a7f9f2ee51e0e401b40b506ed6b0264d22803
Daniel Elvey <delvey21@hotmail.co.uk>
2024-12-12 12:55:27 +0000

    Fixed setting progress by adding loading with loading spinner

--- app/dashboard/videos/[name]/watch/page.tsx
+++ app/dashboard/videos/[name]/watch/page.tsx
@@ -4,6 +4,7 @@ import { notFound, usePathname, useSearchParams } from "next/navigation";
 import Player from "next-video/player";
 import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { createClient } from "@/utils/supabase/client";
+import { TailSpin } from "react-loader-spinner";
 
 // import { Metadata } from "next";
 
@@ -26,19 +27,28 @@ export default function Watch() {
   const videoUrl = searchParams.get("videoUrl");
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [videoDbInfo, setVideoDbInfo] = useState<videoInfoFromDb>();
+  const [loading, setLoading] = useState(true);
   const pathname = usePathname();
   const videoName = pathname.split("/")[3];
 
+  if (!videoUrl) {
+    notFound();
+  }
+
   useLayoutEffect(() => {
     async function fetchVideoInfo() {
-      const { data } = await supabase
+      const { data, error } = await supabase
         .from("videos")
         .select()
         .eq("id", videoName);
-      if (data !== null) {
+      if (data && data.length > 0) {
         setVideoDbInfo(data[0]);
+        setLoading(false);
+      } else {
+        console.error("Error fetching video data from db: ", error);
       }
     }
+
     fetchVideoInfo();
   }, [supabase, videoName]);
 
@@ -62,30 +72,47 @@ export default function Watch() {
 
   useEffect(() => {
     // For setting the playback location of the video loaded.
-    const video = videoRef.current;
-    if (video) {
-      const handleLoadedMetadata = () => {
-        // This should be updated to equal "Progress" from db.
+    if (videoDbInfo && videoRef.current) {
+      const video = videoRef.current;
 
+      const handleLoadedMetadata = () => {
         video.currentTime = videoDbInfo!.progress;
+        // video.pause();
       };
-      video.addEventListener("loadedmetadata", handleLoadedMetadata);
+
+      if (video.readyState >= 1) {
+        video.currentTime = videoDbInfo.progress;
+        // video.pause();
+      } else {
+        video.addEventListener("loadedmetadata", handleLoadedMetadata);
+      }
+
       return () => {
         video.removeEventListener("loadedmetadata", handleLoadedMetadata);
       };
     }
   }, [videoDbInfo]);
 
-  if (!videoUrl) {
-    notFound();
-  }
-
   return (
     <main>
-      {videoUrl ? (
-        <Player src={videoUrl} ref={videoRef} />
+      {!loading && videoUrl && videoDbInfo ? (
+        <Player style={{ display: "grid", width: "100%" }} ref={videoRef}>
+          <source src={videoUrl} type="video/mp4" />
+        </Player>
       ) : (
-        <h1 className="text-3xl">Link brok</h1>
+        <div>
+          <h1 className="text-2xl text-primary-foreground">Loading Video...</h1>
+          <div className="absolute top-[25%] left-1/2">
+            <TailSpin
+              visible={true}
+              height="120"
+              width="120"
+              color="#4fa94d"
+              ariaLabel="tail-spin-loading"
+              radius="1"
+            />
+          </div>
+        </div>
       )}
     </main>
   );
