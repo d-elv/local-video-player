1389ecbd27a062f7bdbbc1f82692b594c59a2e15
Daniel Elvey <delvey21@hotmail.co.uk>
2024-12-18 18:34:25 +0000

    Confirm page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ["class", "class"],
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx,mdx}",
     "./components/**/*.{ts,tsx,mdx}",
