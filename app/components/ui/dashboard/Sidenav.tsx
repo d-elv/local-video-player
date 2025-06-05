4f22ccb0a03b04fa29b8245c16b2a2e592be0e82
Daniel Elvey <delvey21@hotmail.co.uk>
2025-06-05 17:30:49 +0100

    created composition ui components for sidebar

--- app/components/ui/dashboard/Sidenav.tsx
+++ app/components/ui/dashboard/Sidenav.tsx
@@ -1,9 +1,29 @@
 import { CassetteTape, FilmIcon, HouseIcon, PowerIcon } from "lucide-react";
-import Link from "next/link";
+import Link, { LinkProps } from "next/link";
 import { SignOutButton } from "@clerk/nextjs";
 import { ThemeSwitch } from "@/app/components/ui/shared/DarkModeToggle";
 
 export default async function SideNav() {
+  function NavText({ children }: { children: React.ReactNode }) {
+    return <p className="pl-2">{children}</p>;
+  }
+
+  function NavButton({
+    href,
+    children,
+  }: LinkProps & { children: React.ReactNode }) {
+    return (
+      <Link
+        href={href}
+        className="flex justify-center w-full h-full md:justify-start md:h-10 hover:bg-accent hover:rounded-tl-md md:hover:rounded-tr-md md:hover:rounded-tl-md transition-colors"
+      >
+        <div className="flex rounded-md items-center text-primary-foreground md:p-2 md:w-full">
+          {children}
+        </div>
+      </Link>
+    );
+  }
+
   return (
     <div className="flex h-16 flex-shrink-0 md:h-svh md:flex-col md:py-4 md:px-2 md:w-44">
       <Link
@@ -16,33 +36,19 @@ export default async function SideNav() {
         <div className="bg-[#000000] grow rounded-tr-md md:rounded-br-md"></div>
       </Link>
       <div className="flex w-full h-full justify-around rounded-tl-md rounded-tr-md ml-2 mr-2 bg-secondary text-background md:rounded-md md:ml-0 md:mt-2 md:mb-2 md:items-start md:justify-start md:flex-col">
-        <Link
-          href="/dashboard"
-          className="flex justify-center w-full h-full md:justify-start md:h-10 hover:bg-accent hover:rounded-tl-md md:hover:rounded-tr-md md:hover:rounded-tl-md transition-colors"
-        >
-          <div className="flex rounded-md items-center text-primary-foreground md:p-2 md:w-full">
-            <HouseIcon />
-            <p className="pl-2">Home</p>
-          </div>
-        </Link>
-        <Link
-          href="/dashboard/history"
-          className="flex justify-center w-full h-full md:h-10 hover:bg-accent transition-colors"
-        >
-          <div className="flex rounded-md justify-center items-center md:justify-start  text-primary-foreground md:p-2 md:w-full">
-            <FilmIcon />
-            <p className="pl-2">History</p>
-          </div>
-        </Link>
-        <Link
-          href="/dashboard/demo"
-          className="flex justify-center w-full h-full md:h-10 hover:bg-accent transition-colors"
-        >
-          <div className="flex rounded-md justify-center items-center md:justify-start  text-primary-foreground md:p-2 md:w-full">
-            <CassetteTape />
-            <p className="pl-2">Demo</p>
-          </div>
-        </Link>
+        <NavButton href="/dashboard">
+          <HouseIcon />
+          <NavText>Home</NavText>
+        </NavButton>
+        <NavButton href="/history">
+          <FilmIcon />
+          <NavText>History</NavText>
+        </NavButton>
+        <NavButton href="/dashboard/demo">
+          <CassetteTape />
+          <NavText>Demo</NavText>
+        </NavButton>
+
         <div className="flex items-center justify-center w-full h-full text-primary-foreground md:justify-start md:h-10 hover:bg-accent hover:rounded-tr-md md:hover:rounded-tl-none md:hover:rounded-tr-none md:hover:rounded-bl-md md:hover:rounded-br-md md:mt-auto transition-colors">
           <ThemeSwitch />
         </div>
